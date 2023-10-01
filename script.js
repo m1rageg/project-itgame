4c173a58e520cc02dbee59d9a2c0956ec24c149b
m1rageg <egor.sergei.demin153@gmail.com>
2023-10-01 22:48:27 +0300

    rework

--- script.js
+++ script.js
@@ -1,20 +1,279 @@
-class Main_hero{
+class MainHero{
     constructor(name,){
         this.name = name;
-        this.health = null;
-        this.happiness = null;
-        this.food = null;
+        this.health = 50;
+        this.happiness = 50;
+        this.food = 50;
         this.exp = null;
+        this.usd = 0
+        this.uah = 0
     }
-    set_food(){}
-    set_health(){}
-    set_happiness(){}
-    set_exp(){}
 }
 
+let rocketLeague = new MainHero('Rocket league')
 
-function setsBar (id, value) {
+function displayHeroStats (hero) {
+    setBar ("health", hero.health)
+    setBar ("happiness", hero.happiness)
+    setBar ("food", hero.food)
+    const statsUsd = document.getElementById("stats__dollars")
+    const statsUah = document.getElementById("stats__local-money")
+    statsUsd.innerText = `${hero.usd.toFixed(2)} $`
+    statsUah.innerHTML = `${hero.uah.toFixed(2)} &#8372;`
+}
+
+// array with hero roles matching bottom levels
+arrayOfMainHeroes = [
+    {
+        name: "poorstudent",
+        value: 0
+    },
+    {
+        name: "student",
+        value: 20
+    },
+    {
+        name: "trainee",
+        value: 40
+    },
+    {
+        name: "junior",
+        value: 60
+    },
+    {
+        name: "middle",
+        value: 80
+    },
+    {
+        name: "senior",
+        value: 100
+    }]
+
+// function to set value to bar (health / happiness / food / exp)
+function setBar (id, value) {
     const bar = document.getElementById(`${id}__meter`)
     bar.value = value
 }
 
+// function to set character picture depending on hero role
+function setPicture(nameOfPicture){
+    const img = document.querySelector('.character__img');
+    let newWay = `img/${nameOfPicture}.png`
+    return img.src = newWay
+    }
+
+// function to match hero level to hero role
+function checkHeroLevel(hero) {
+    arrayOfMainHeroes.forEach(element => {
+        if(element.value <= hero.exp){
+            setPicture(element.name)
+        }
+    });
+}
+
+// array of possible items to be rendered
+const arrayOfRandomItems = [
+    {
+        name: "Gym workout",
+        price: 10,
+        currency: "USD",
+        buff: 15,
+        category: "health",
+        isMonthly: false,
+        isAvailable: true
+    }
+]
+// function to render list items in the menu
+function renderMenu (menuName) {
+    const healthList = document.getElementById(`${menuName}__items__li`)
+    arrayOfRandomItems.forEach((item) => {
+        if (item.isAvailable && item.category === menuName) {
+            const listItem = document.createElement("li")
+            listItem.innerText = `${item.name} (+${item.buff}, -${item.price}) ${item.currency}`
+            healthList.appendChild(listItem)
+        }
+    })
+}
+
+// function for changing date
+function incrementDate(date) {
+    date.setDate(date.getDate() + 1);
+}
+
+let gameDate = new Date(2000, 0, 1)
+let countDays = 0
+
+
+// function for increasing current game date
+function increaseGameDate() {
+    incrementDate(gameDate);
+    const month = gameDate.toLocaleString('eng', { month: 'long' }); 
+    const day = gameDate.getDate();
+    const year = gameDate.getFullYear();
+    countDays +=1
+    document.querySelector('.count__days').innerHTML = `Current Date: <br><span>${day}-${month}-${year}</span><br>Total days lived: ${countDays}`;
+    if (countDays % 10 === 0){
+        generateCurrency()
+        displayExchangeRates()
+    }
+    // rocketLeague.food -= 2
+    // rocketLeague.happiness -= 4
+    // rocketLeague.health -= 1.5
+    if(countDays % 30 === 0 && arrayHappinessItems[3].isMarried === true){
+        rocketLeague.happiness += arrayHappinessItems[3].buffHappiness
+        rocketLeague.food += arrayHappinessItems[3].buffFood
+        rocketLeague.uah -= arrayHappinessItems[3].price
+        displayHeroStats(rocketLeague)
+    }
+    displayHeroStats(rocketLeague)
+    setTimeout(increaseGameDate, 100)
+}
+increaseGameDate()
+//end function for changing date
+
+//function for currency change
+let exchangeRateBuy = 0
+let exchangeRateSell = 0
+function generateCurrency() {
+    exchangeRateBuy = (Math.random() * 4 + 38).toFixed(2)
+    exchangeRateSell = (exchangeRateBuy - (Math.random() * 2)).toFixed(2)
+}
+
+//function to display exchange rates
+function displayExchangeRates () {
+    const buyUsdEl = document.getElementById("buy__usd")
+    const sellUsdEl = document.getElementById("sell__usd")
+    buyUsdEl.innerText = `Buy usd for ${exchangeRateBuy}`
+    sellUsdEl.innerHTML = `Sell usd for ${exchangeRateSell}`
+}
+generateCurrency()
+displayExchangeRates()
+
+// function to convert currencies
+function currencyExchangeRate (value, currency) {
+    if (currency === "USD") {
+        return value * exchangeRateSell
+    } else if (currency === "UAH") {
+        return value * exchangeRateBuy
+    } else {
+        return null
+    }
+}
+
+// function to complete exchange
+function completeExchange (value, currency, person) {
+    const exchangedSum = currencyExchangeRate(value, currency)
+    if (currency === "USD") {
+        person.usd -= +value
+        person.uah += exchangedSum
+    } else if (currency === "UAH") {
+        person.usd += +value
+        person.uah -= exchangedSum
+    } else {
+        console.log(currency)
+        console.log("ERROR")
+    }
+}
+
+function exchangeFormHandler (event) {
+    event.preventDefault();
+    const form = event.target;
+    const currencyInput = form.elements["currency"].value;
+    const sumInput = form.elements["sum"].value;
+    completeExchange(sumInput, currencyInput, rocketLeague)
+    return false
+}
+
+// function to open/close game menus
+let currentMenuId = "character__syka"
+function changeMenuDisplay (menuToOpen) {
+    if (currentMenuId != menuToOpen) {
+        const currentMenu = document.getElementById(currentMenuId)
+        currentMenu.classList.add("no-display")
+        const newMenu = document.getElementById(menuToOpen)
+        newMenu.classList.remove("no-display")
+        currentMenuId = menuToOpen
+    } else if ( currentMenuId === menuToOpen) {
+        const currentMenu = document.getElementById(currentMenuId)
+        currentMenu.classList.add("no-display")
+        const newMenu = document.getElementById("character__syka")
+        newMenu.classList.remove("no-display")
+        currentMenuId = "character__syka"
+    }
+}
+
+
+//function to exit game menus
+function exitMenus(openedMenu){
+    const currentMenu = document.getElementById("character__syka")
+    openedMenu.classList.add("no-display")
+    currentMenu.classList.remove("no-display")
+    currentMenuId = "character__syka"
+
+}
+
+let arrayHappinessItems = [
+    {
+        name: "walk",
+        price: 0,
+        buffHappiness: 5,
+        isMonthly: false,
+        buffFood: -2,
+    },
+    {
+        name: "sleep",
+        price: 0,
+        buffHappiness: 10,
+        isMonthly: false,
+        buffFood: -5,  
+    },
+    {
+        name: "cake",
+        price: 100,
+        buffHappiness: 20,
+        isMonthly: false,
+        buffFood: 10, 
+        buffHealth: 0, 
+    },
+    {
+        name: "marry",
+        price: 10000,
+        buffHappiness: 70,
+        isMonthly: true,
+        isMarried: false,
+        buffFood: 70, 
+    }
+]
+
+//function to logic happiness
+function happinessItem(button){
+    const buttonClass = button.classList[0];
+    switch (buttonClass) {
+        case 'sleep_menu':
+            rocketLeague.happiness += arrayHappinessItems[1].buffHappiness
+            rocketLeague.food += arrayHappinessItems[1].buffFood
+            displayHeroStats(rocketLeague)
+            break;
+        case 'walk_menu':
+            rocketLeague.happiness += arrayHappinessItems[0].buffHappiness
+            rocketLeague.food += arrayHappinessItems[0].buffFood
+            displayHeroStats(rocketLeague)  
+            break;
+        case 'cake_menu':
+            rocketLeague.happiness += arrayHappinessItems[2].buffHappiness
+            rocketLeague.food += arrayHappinessItems[2].buffFood
+            rocketLeague.uah -= arrayHappinessItems[2].price
+            displayHeroStats(rocketLeague)
+            break;
+        case 'marry_menu':
+            button.innerHTML = arrayHappinessItems[3].isMarried === false ? "You are already married!"  : "Marry(+70&#128525 every month, +70&#127828 every month, -10000&#8372 every month)"
+            arrayHappinessItems[3].isMarried = arrayHappinessItems[3].isMarried === false? true : false
+            break;
+        default:
+            console.log('unknown')
+        }
+}
+function healthItem(button){}
+function foodItem(button){}
+function studyItem(button){}
+function workItem(button){}
