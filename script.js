11e3d49505ea99e98a8d4f91ab32ae44d09d15ec
m1rageg <egor.sergei.demin153@gmail.com>
2023-10-03 11:52:31 +0300

    new features

--- script.js
+++ script.js
@@ -10,12 +10,40 @@ class MainHero{
     }
 }
 
-let rocketLeague = new MainHero('Rocket league')
+let tempDaysCourse = 0
+let tempDaysUniversity = 0
+
+let arrayStudyItems = [
+    {
+        name: "book",
+        price: 0,
+        exp: 300,
+        days: 0,
+        
+    },
+    {
+        name: "course",
+        price: 7000,
+        exp: 1500,
+        days: 90,
+        isBought: false
+    },
+    {
+        name: "university",
+        price: 0,
+        exp: 5000,
+        days: 1095,
+        isBought: false
+    }
+]
+
+let rocketLeague = new MainHero('Rocket League')
 
 function displayHeroStats (hero) {
     setBar ("health", hero.health)
     setBar ("happiness", hero.happiness)
     setBar ("food", hero.food)
+    setBar("experience", currentExp)
     const statsUsd = document.getElementById("stats__dollars")
     const statsUah = document.getElementById("stats__local-money")
     statsUsd.innerText = `${hero.usd.toFixed(2)} $`
@@ -54,6 +82,24 @@ function setBar (id, value) {
     const bar = document.getElementById(`${id}__meter`)
     bar.value = value
 }
+
+
+let currentLevel = 1
+let currentExp = 0
+let needExp = 1000
+function renderExp(countExp){
+    const personLevel = document.querySelector(".person__level")
+    const needExpDisplay = document.querySelector(".need__exp")
+    let tempExp = currentExp + countExp
+    while (tempExp >= 1000) {
+        currentLevel += 1;
+        tempExp -= 1000;
+    }
+    currentExp = tempExp
+    personLevel.textContent = `Current level: ${currentLevel}`
+    needExpDisplay.textContent = `${currentExp}/${needExp}`
+}
+
 
 // function to set character picture depending on hero role
 function setPicture(nameOfPicture){
@@ -125,6 +171,27 @@ function increaseGameDate() {
         rocketLeague.uah -= arrayHappinessItems[3].price
         displayHeroStats(rocketLeague)
     }
+    if(countDays % 30 === 0 && arrayHealthItems[5].isHired === true){
+        rocketLeague.health += arrayHealthItems[5].buffHealth
+        rocketLeague.uah -= arrayHealthItems[5].price
+        displayHeroStats(rocketLeague)
+    }
+    if(countDays % 30 === 0 && arrayFoodItems[3].isHired === true){
+        rocketLeague.food += arrayFoodItems[3].buffFood
+        rocketLeague.uah -= arrayFoodItems[3].price
+        displayHeroStats(rocketLeague)
+    }
+    if (arrayStudyItems[1].isBought === true && tempDaysCourse === countDays){
+        renderExp(arrayStudyItems[1].exp);
+        displayHeroStats(rocketLeague)
+        arrayStudyItems[1].isBought = false
+    }
+    if (arrayStudyItems[2].isBought === true && tempDaysUniversity === countDays){
+        renderExp(arrayStudyItems[2].exp);
+        displayHeroStats(rocketLeague)
+        arrayStudyItems[2].isBought = false
+    }
+
     displayHeroStats(rocketLeague)
     setTimeout(increaseGameDate, 100)
 }
@@ -268,12 +335,230 @@ function happinessItem(button){
         case 'marry_menu':
             button.innerHTML = arrayHappinessItems[3].isMarried === false ? "You are already married!"  : "Marry(+70&#128525 every month, +70&#127828 every month, -10000&#8372 every month)"
             arrayHappinessItems[3].isMarried = arrayHappinessItems[3].isMarried === false? true : false
+            arrayFoodItems[4].isMarried = arrayFoodItems[4].isMarried === false? true : false
+            if(arrayHappinessItems[3].isMarried === true || arrayFoodItems[4].isMarried === true){
+                let temp = document.querySelectorAll(".marry_menu")
+                temp.forEach((elem) => elem.innerHTML = "You are already married!")
+            }else {
+                let temp = document.querySelectorAll(".marry_menu")
+                temp.forEach((elem) => elem.innerHTML = "Marry(+70&#128525 every month, +70&#127828 every month, -10000&#8372 every month)")  
+            }
             break;
         default:
             console.log('unknown')
         }
 }
-function healthItem(button){}
-function foodItem(button){}
-function studyItem(button){}
+
+function updateMarryButton() {
+    let temp = document.querySelectorAll('marry_menu')
+    if(arrayHappinessItems[3].isMarried === false && arrayHealthItems[4].isMarried === false){  
+        temp.innerHTML = "Marry(+70&#128525 every month, +70&#127828 every month, -10000&#8372 every month)"
+    } else if(arrayHappinessItems[3].isMarried === true || arrayHealthItems[4].isMarried === true){
+        temp.innerHTML = "You are already married!"
+    }
+}
+
+let arrayHealthItems = [
+    {
+        name: "workout",
+        price: 0,
+        buffHealth: 5,
+        buffHappiness: 0,
+        buffFood: -3,
+        isMonthly: false,
+        
+    },
+    {
+        name: "pill",
+        price: 0,
+        buffHealth: 15,
+        buffHappiness: -5,
+        buffFood: 0,
+        isMonthly: false,  
+    },
+    {
+        name: "doctor",
+        price: 1000,
+        buffHealth: 25,
+        buffHappiness: -5,
+        buffFood: 0,
+        isMonthly: false, 
+    },
+    {
+        name: "hospital",
+        price: 4000,
+        buffHealth: 50,
+        buffHappiness: 0,
+        buffFood: -15,
+        isMonthly: false, 
+    },
+    {
+        name: "abroad",
+        price: 4000,
+        currency: "usd",
+        buffHealth: 100,
+        buffHappiness: 0,
+        buffFood: 0,
+        isMonthly: false, 
+    },
+    {
+        name: "personaldoctor",
+        price: 25000,
+        currency: "uah", 
+        buffHealth: 70,
+        buffHappiness: 0,
+        buffFood: 0,
+        isMonthly: true,
+        isHired: false, 
+    }
+]
+function healthItem(button){
+    const buttonClass = button.classList[0];
+    switch (buttonClass) {
+        case 'workout_menu':
+            rocketLeague.health += arrayHealthItems[0].buffHealth
+            rocketLeague.food += arrayHealthItems[0].buffFood
+            displayHeroStats(rocketLeague)
+            break;
+        case 'pill_menu':
+            rocketLeague.health += arrayHealthItems[1].buffHealth
+            rocketLeague.happiness += arrayHealthItems[1].buffHappiness
+            displayHeroStats(rocketLeague)
+            break;
+        case 'doctor_menu':
+            rocketLeague.health += arrayHealthItems[2].buffHealth
+            rocketLeague.food += arrayHealthItems[2].buffHappiness
+            rocketLeague.uah -= arrayHealthItems[2].price
+            displayHeroStats(rocketLeague)
+            break;
+        case 'hospital_menu':
+            rocketLeague.health += arrayHealthItems[3].buffHealth
+            rocketLeague.food += arrayHealthItems[3].buffFood
+            rocketLeague.uah -= arrayHealthItems[3].price
+            displayHeroStats(rocketLeague)
+            break;
+        case 'abroad_menu':
+            rocketLeague.health += arrayHealthItems[4].buffHealth
+            rocketLeague.usd -= arrayHealthItems[4].price
+            displayHeroStats(rocketLeague)
+            break;
+        case 'personaldoctor_menu':
+            button.innerHTML = arrayHealthItems[5].isHired === false ? "The doctor is already hired!"  : "Hire a personal doctor(+70&#10084; every month, -25000&#8372 every month)"
+            arrayHealthItems[5].isHired = arrayHealthItems[5].isHired === false? true : false
+            break;
+        default:
+            console.log('unknown')
+        }
+}
+
+let arrayFoodItems = [
+    {
+        name: "meal",
+        price: 0,
+        buffHealth: 0,
+        buffHappiness: -5,
+        buffFood: 20,
+        isMonthly: false,
+        
+    },
+    {
+        name: "fastfood",
+        price: 300,
+        buffHealth: -10,
+        buffHappiness: 0,
+        buffFood: 30,
+        isMonthly: false,  
+    },
+    {
+        name: "restaurant",
+        price: 1000,
+        buffHealth: 0,
+        buffHappiness: 0,
+        buffFood: 40,
+        isMonthly: false, 
+    },
+    {
+        name: "personalchef",
+        price: 20000,
+        buffHealth: 0,
+        buffHappiness: 0,
+        buffFood: 100,
+        isMonthly: true,
+        isHired: false 
+    },
+    {
+        name: "marry",
+        price: 10000,
+        buffHealth: 0,
+        buffHappiness: 70,
+        buffFood: 70,
+        isMonthly: true,
+        isMarried: false 
+    }
+]
+
+function foodItem(button){
+    const buttonClass = button.classList[0];
+    switch (buttonClass) {
+        case 'meal_menu':
+            rocketLeague.happiness += arrayFoodItems[0].buffHappiness
+            rocketLeague.food += arrayFoodItems[0].buffFood
+            displayHeroStats(rocketLeague)
+            break;
+        case 'fastfood_menu':
+            rocketLeague.uah -= arrayFoodItems[1].price
+            rocketLeague.health += arrayFoodItems[1].buffHealth
+            rocketLeague.food += arrayFoodItems[1].buffFood
+            displayHeroStats(rocketLeague)
+            break;
+        case 'restaurant_menu':
+            rocketLeague.uah -= arrayFoodItems[2].price
+            rocketLeague.food += arrayFoodItems[2].buffFood
+            displayHeroStats(rocketLeague)
+            break;
+        case 'personalchef_menu':
+            button.innerHTML = arrayFoodItems[3].isHired === false ? "The chef is already hired!"  : "Hire a personal chef(+100&#127828 every month, -20000&#8372)"
+            arrayFoodItems[3].isHired = arrayFoodItems[3].isHired === false? true : false
+            break;
+        case 'marry_menu': 
+            button.innerHTML = arrayFoodItems[4].isMarried === false ? "You are already married!"  : "Marry(+70&#128525 every month, +70&#127828 every month, -10000&#8372 every month)"
+            arrayFoodItems[4].isMarried = arrayFoodItems[4].isMarried === false? true : false
+            arrayHappinessItems[3].isMarried = arrayHappinessItems[3].isMarried === false ? true : false
+            if(arrayHappinessItems[3].isMarried === true || arrayFoodItems[4].isMarried === true){
+                let temp = document.querySelectorAll(".marry_menu")
+                temp.forEach((elem) => elem.innerHTML = "You are already married!")
+            } else{
+                let temp = document.querySelectorAll(".marry_menu")
+                temp.forEach((elem) => elem.innerHTML = "Marry(+70&#128525 every month, +70&#127828 every month, -10000&#8372 every month)")  
+            }
+            break;
+        default:
+            console.log('unknown')
+    }
+}
+
+
+
+function studyItem(button){
+    const buttonClass = button.classList[0];
+    switch (buttonClass) {
+        case "book_menu":
+            rocketLeague.exp += arrayStudyItems[0].exp
+            break
+        case "course_menu":
+            arrayStudyItems[1].isBought = arrayStudyItems[1].isBought === false ? true : false
+            rocketLeague.uah -= arrayStudyItems[1].price;
+            tempDaysCourse = countDays + arrayStudyItems[1].days
+            break
+        case "university_menu":
+            arrayStudyItems[2].isBought = arrayStudyItems[2].isBought === false ? true : false
+            tempDaysUniversity = countDays + arrayStudyItems[2].days
+            break
+        default:
+            break
+    }
+}
+
+
+
 function workItem(button){}
